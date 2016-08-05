fc70131dca5d3c58e78988bcda4ab51304e023ad
Molly <mollykmcglone@gmail.com>
2016-08-05 15:49:04 -0700

    add image to jumbotron, center text throughout, style buttons

--- js/search-interface.js
+++ js/search-interface.js
@@ -2,20 +2,33 @@ var Search = require('./../js/search.js').searchModule;
 var searchObject = new Search();
 
 var displaySearch = function(username, public_repos) {
-  $('#title').text(username + " has " + public_repos + " public repositories.");
-  $('#showButton').html("<button class='btn-success' id='seeRepos'>See Repos</button>");
-  $('#seeRepos').click(function(event){
-    searchObject.getUserRepos(username, displayRepos);
-  });
+  var count = 0;
+  $('#showResults').html("<h3>" + username + " has " + public_repos + " public repositories.</h3>");
+    $('#seeRepos').show();
+    $('#seeRepos').click(function(event){
+      if (count === 0) {
+        searchObject.getUserRepos(username, displayRepos);
+        count += 1;
+      }
+      $('#showRepos').show();
+      $('#seeRepos').hide();
+      $('#hideRepos').show();
+    });
 };
 
 var displayRepos = function(username, repos) {
-  $('#title2').text("Here are the 30 repos they've updated most recently:");
+  $('#showRepos').append('<br><ul>');
   repos.forEach(function(repo) {
     if (repo.description === null) {
-      repo.description = "No description provided"
+      repo.description = "No description provided";
     }
     $('#showRepos').append('<li><a href=' + repo.html_url + '>' + repo.name + '</a> ' + repo.description + '</li>');
+  });
+  $('#showRepos').append('</ul></div>');
+  $('#hideRepos').click(function(event){
+    $('#showRepos').hide();
+    $('#hideRepos').hide();
+    $('#seeRepos').show();
   });
 };
 
@@ -23,8 +36,10 @@ $(document).ready(function() {
   $('.search-form').submit(function(event){
     event.preventDefault();
     var username = $('#username').val();
+    $('#username').val("");
+    $('#showRepos').text("");
+    $('#hideRepos').hide();
     console.log(username);
     searchObject.userSearch(username, displaySearch);
-
   });
 });
