05cc465899461e9eee3cec6fad8663d6ace7b39d
Molly <mollykmcglone@gmail.com>
2016-08-05 12:34:53 -0700

    update displayRepos to include links to most recent 30 projects

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,27 +1,27 @@
 var Search = require('./../js/search.js').searchModule;
+var searchObject = new Search();
 
 var displaySearch = function(username, public_repos) {
   $('#title').text(username + " has " + public_repos + " public repositories.");
   $('#showButton').html("<button class='btn-success' id='seeRepos'>See Repos</button>");
+  $('#seeRepos').click(function(event){
+    searchObject.getUserRepos(username, displayRepos);
+  });
 };
 
 var displayRepos = function(username, repos) {
-  $('#title').append("Here are the 30 repos they've updated most recently:");
+  $('#title2').text("Here are the 30 repos they've updated most recently:");
   repos.forEach(function(repo) {
-    $('#showRepos').append("<li>" + repo.name + "</li>");
+    $('#showRepos').append("<li><a href='" + repo.html_url +"'</a>" + repo.name + "</li>");
   });
 };
 
 $(document).ready(function() {
-  var searchObject = new Search();
   $('.search-form').submit(function(event){
     event.preventDefault();
     var username = $('#username').val();
     console.log(username);
     searchObject.userSearch(username, displaySearch);
-  });
 
-  $('#seeRepos').click(function(event){
-    searchObject.getRepos(username, displayRepos);
   });
 });
