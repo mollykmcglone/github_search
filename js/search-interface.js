2a4325b2c27f6823afaca5ceaa8c03806ac2a9df
Molly <mollykmcglone@gmail.com>
2016-08-05 12:16:21 -0700

    create Search model with userSearch and getRepos prototypes

--- js/search-interface.js
+++ js/search-interface.js
@@ -1,11 +1,27 @@
 var Search = require('./../js/search.js').searchModule;
 
+var displaySearch = function(username, public_repos) {
+  $('#title').text(username + " has " + public_repos + " public repositories.");
+  $('#showButton').html("<button class='btn-success' id='seeRepos'>See Repos</button>");
+};
+
+var displayRepos = function(username, repos) {
+  $('#title').append("Here are the 30 repos they've updated most recently:");
+  repos.forEach(function(repo) {
+    $('#showRepos').append("<li>" + repo.name + "</li>");
+  });
+};
+
 $(document).ready(function() {
   var searchObject = new Search();
   $('.search-form').submit(function(event){
     event.preventDefault();
     var username = $('#username').val();
     console.log(username);
-    searchObject.doSearch(username);
+    searchObject.userSearch(username, displaySearch);
+  });
+
+  $('#seeRepos').click(function(event){
+    searchObject.getRepos(username, displayRepos);
   });
 });
--- js/search.js
+++ js/search.js
@@ -3,12 +3,21 @@ var apiKey = require('./../.env').apiKey;
 Search = function(){
 };
 
-Search.prototype.doSearch = function(username) {
+Search.prototype.getUserRepos = function(username, displayRepos) {
+    $.get('https://api.github.com/users/' + username + '/repos?&sort=updated/access_token=' + apiKey).then(function(response){
+    console.log(response);
+    displayRepos(username, response);
+  }).fail(function(error) {
+    $('.showResults').text(error.responseJSON.message);
+  });
+};
+
+Search.prototype.userSearch = function(username, displaySearch) {
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
     console.log(response);
-    // $('.results').text("Over the past year there were " + response.proximity + " " + manufacturer + " bikes stolen in Portland.");
+    displaySearch(username, response.public_repos);
   }).fail(function(error) {
-    $('.results').text(error.responseJSON.message);
+    $('.showResults').text(error.responseJSON.message);
   });
 };
 
